cb489eeb6a4dfecd984b568c7ceb875f1a938da8
Keith Shaw <github@keithws.net>
2015-04-22 03:58:28 -0700

    Add safari extension code to block alert, confirm, and prompt.

---
+++ curb-your-alerts.safariextension/curb.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+	// inject script into document
+	// to override alert, confirm, and prompt
+	var e = document.createElement("script");
+	e.innerText = "alert = confirm = prompt = function (message) { throw new Error(message); };";
+	document.body.insertBefore(e, document.body.firstChild);
+
+});
